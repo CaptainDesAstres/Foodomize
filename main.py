b364c854a13e673a53e2eb468078f7eb51bebd1e
Captain DesAstres <captaindesastres@gmail.com>
2017-04-21 18:43:12 +0200

    create app main scope

---
+++ main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3.4
+# -*-coding:Utf-8 -*
+'''App main scope'''
+
+
+def main():
+	'''app main function'''
+	
+	while(True):
+		menu = input('Menu ? (h for help):').strip()
+		
+		if(menu in ['exit', 'o', 'out', 'q', 'quit']):
+			break
+		elif(menu in ['help', 'h'])
+			print('''Help:
+
+Press enter to quit Help
+
+In main menu:
+Type:                    For:
+h or help                Get some help
+q or quit                Quit the application
+''')
+	
+	
+	
+	
+	
+	
+	
+	
+
+main()
