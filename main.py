6b243a9b13171ea8718ef237011ce73c21f57deb
Captain DesAstres <captaindesastres@gmail.com>
2017-04-21 19:02:54 +0200

    create main Element to list future element

--- main.py
+++ main.py
@@ -2,9 +2,12 @@
 # -*-coding:Utf-8 -*
 '''App main scope'''
 import os
+from Element import Element
 
 def main():
 	'''app main function'''
+	
+	mainlist= Element()
 	
 	while(True):
 		os.system('clear')# clear terminal output
