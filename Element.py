e9214682ea54fe141f8115c5561093b1bde65726
Captain DesAstres <captaindesastres@gmail.com>
2017-04-21 18:30:43 +0200

    set Element init method

--- Element.py
+++ Element.py
@@ -6,9 +6,24 @@
 class Element:
 	'''Element base object'''
 	
-	def __init__(self, start, init):
+	def __init__(self, name, coef=1, kind='group'):
 		'''initialize Element'''
+		self.name = name
+		self.kind = kind
 		
+		
+		if ( type(coef) == int ):
+			self.coef = [coef]*12
+		else
+			l=len(coef) 
+			if l == 12 :
+				self.coef = coef
+			elif l in [6;4;3;2]:
+				l = int(12 / l)
+				self.coef = []
+				while ( len(coef) != 0 ):
+					self.coef.extend( [ coef.pop(0) ] * l )
+			
 	
 	
 	
