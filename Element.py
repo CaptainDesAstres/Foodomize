3a7e846d1eebfeaa9bccd5ca107bb3e043e38128
Captain DesAstres <captaindesastres@gmail.com>
2017-04-21 18:13:06 +0200

    add Element class

---
+++ Element.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3.4
+# -*-coding:Utf-8 -*
+'''Element base object'''
+
+
+class Element:
+	'''Element base object'''
+	
+	def __init__(self, start, init):
+		'''initialize Element'''
+		
+	
+	
+	
+	
+	def __str__(self):
+		'''return Element in a string form'''
+		return 'Not implemented'
+	
+	
+	
+	
+
