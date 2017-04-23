00f81085ced4d30ce7199786375baa2b9e603891
Captain DesAstres <captaindesastres@gmail.com>
2017-04-23 13:59:02 +0200

    save when using the quitting shortcut

--- Element.py
+++ Element.py
@@ -114,8 +114,16 @@ h or help                Get some help'''+quit)
 				if menu < len( self.sub ):
 					quit = self.sub[menu].menu()
 				
-				if quit:
-					return True
+				if(quit):
+					if self.name != 'Main':
+						return True
+					else:
+						# save before to quit
+						if(self.save()):
+							return True
+						else:
+							if( input('Quit confirmation (type y or yes):').strip().lower() in [ 'y', 'yes' ] ):
+								return True
 	
 	
 	
